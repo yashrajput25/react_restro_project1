c46979bd156881b3d15bbaf8ed1b9410e4ed02eb
Yash Rajput <yashrajputyr10@gmail.com>
2024-12-19 20:51:15 +0530

    Initial commit

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import Dosa from "../images/dosa.jpg";
+import Chola from "../images/chola.webp";
+import Idli from "../images/idli.jpg";
+import MasalaDosa from "../images/masala.jpg";
+import Paneer from "../images/paneer.jpg";
+import Gujrati from "../images/thali.jpg";
+
+export const MenuList = [
+  {
+    name: "Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Dosa,
+    price: 200,
+  },
+  {
+    name: "Chola Bhatura",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Chola,
+    price: "250",
+  },
+  {
+    name: "Idli Sambhar",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Idli,
+    price: 300,
+  },
+  {
+    name: "Masala Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: MasalaDosa,
+    price: 100,
+  },
+  {
+    name: "Shahi Paneer",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Paneer,
+    price: 400,
+  },
+  {
+    name: "Deluxe Thali",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Gujrati,
+    price: 500,
+  },
+];
